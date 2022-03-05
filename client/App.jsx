1f9c7399b99634e9d14c8fc438578e42320f2c63
david <github@djs.gg>
2022-03-05 17:31:20 -0500

    fetch imageURL almost working

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-//import Poster from './components/Poster.jsx';
+import Poster from './components/Poster.jsx';
 
 const App = () => {
   return(
    <div> 
      <h1>hello world</h1>
+     <Poster />
    </div>
   );
 };
--- client/components/Poster.jsx
+++ client/components/Poster.jsx
@@ -2,39 +2,48 @@ import React from 'react'
 import * as privateVars from '../constants/private.js'
 const Poster = props => {
   
-    let baseURL = 'https://api.themovieb.org/3/';
+    let baseURL = 'https://api.themoviedb.org/3/';
     let configData = null;
     let baseImageURL = null;
+    let imageURL = 'hello';
+    console.log('first image url', imageURL)
+    let posterSize = 'w92';
 
     let getConfig = function(keyword = 'jaws'){
-     let url = ''.concat(baseURL, 'configuration?api_keys', privateVars.apiKey);
-     fetch(url)
-       .then((result) => {
+      let url = ''.concat(baseURL, 'configuration?api_key=', privateVars.apiKey);
+      fetch(url)
+      .then((result) => {
+          console.log('first fetch result', result)
           return result.json();
        })
        .then((data)=>{
-         baseImageURL = data.images.secure_base_URL;
+         baseImageURL = data.images.secure_base_url;
+         imageURL += baseImageURL + posterSize;
          configData = data.images;
-         runSearch(keyword)
+         return runSearch(keyword)
        })
-       .catch(function(err){alert(err)});
+       .catch(function(err){console.log(err)});
     }
-
+    
+    // console.log(getConfig());
+    
     let runSearch = function(keyword){
-      let url=''.concat(baseURL,'search/movie?api_keys=', APIKEY, '&query=', keyword);
+      let url=''.concat(baseURL,'search/movie?api_key=', privateVars.apiKey, '&query=', keyword);
       fetch(url)
         .then(result => result.json())
         .then((data) => {
-           // document.getElementById('output').innerHTML = JSON.stringify(data,null,4);
+           console.log(data);
+           const posterPath = data.results[0].poster_path;
+           return imageURL += posterPath;      
         })
     }
 
     getConfig()
-
+    console.log('last image URL', imageURL)
     return (
      <div>
-       <img src={movieObj.poster_path} alt={} />
-       <h2>{}</h2>
+       <h2>Poster Component</h2>
+       <img src={imageURL} alt={"jaws"} />
      </div>    
     );
 }
