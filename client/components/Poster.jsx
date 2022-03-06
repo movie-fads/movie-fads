1ba227fd50932723789ee99483df58dd6c717ae7
david <github@djs.gg>
2022-03-05 19:50:59 -0500

    refactor Poster.jsx

--- client/components/Poster.jsx
+++ client/components/Poster.jsx
@@ -1,45 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import * as privateVars from '../constants/private.js'
+
 const Poster = props =>  {
-  let [BaseURL, setBaseURl] = useState('https://api.themoviedb.org/3/');
-  let [ConfigData, setConfigData] = useState(null);
-  let [BaseImageURL, setBaseImageURL] = useState(null);
-  let [ImageURL, setImageURL] = useState('');
-  let [PosterSize, setPosterSize] = useState('w92');
+  let [BaseImageURL, setBaseImageURL] = useState('');
   let [PosterPath, setPosterPath] = useState('');
+  let [setConfigData] = useState('');
+  let [BaseURL] = useState('https://api.themoviedb.org/3/');
+  let [PosterSize] = useState('w92');
   
 useEffect(() => {
-    let configUrl = ''.concat(BaseURL, 'configuration?api_key=', privateVars.apiKey);
-    console.log('configURL', configUrl)
-    fetch(configUrl)
-    .then(result => {
-      console.log('Result 1', result)
-      return result.json();
-    })
-    .then(data =>{
-      console.log('Data 1', data);
-      setBaseImageURL(data.images.secure_base_url);
-      setImageURL(''.concat(BaseImageURL,PosterSize));
-      console.log('ImageURL999', ImageURL)
-      setConfigData(data.images);
-      console.log('Here');
-    });
-    let url = ''.concat(BaseURL,'search/movie?api_key=', privateVars.apiKey, '&query=', 'jaws');
-    fetch(url)
+  let configUrl = ''.concat(BaseURL, 'configuration?api_key=', privateVars.apiKey);
+  fetch(configUrl)
+  .then(result => result.json())
+  .then(data =>{
+    setBaseImageURL(data.images.secure_base_url);
+    setConfigData(data.images);
+  });
+  
+  let url = ''.concat(BaseURL,'search/movie?api_key=', privateVars.apiKey, '&query=', 'jaws');
+  fetch(url)
     .then(result => result.json())
-    .then (data => {
-      console.log('dta9999', data)
-      setPosterPath(data.results[0].poster_path);
-      setImageURL(ImageURL  + PosterPath);
-    });
+    .then (data => setPosterPath(data.results[0].poster_path));
   },[]);
-  console.log('imageURL1', ImageURL)
 
-    return (
-     <div>
-       <h2>Poster Component</h2>
-       <img src={BaseImageURL + PosterSize + PosterPath} alt={"jaws"} />
-     </div>    
-    );
+return (
+  <div>
+    <h2>Poster Component</h2>
+    <img src={BaseImageURL + PosterSize + PosterPath} alt={"jaws"} />
+  </div>    
+);
 }
 export default Poster;
