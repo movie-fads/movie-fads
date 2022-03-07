d86057fb848a42a488c89a5ee4967e1db3704d59
david <github@djs.gg>
2022-03-07 12:18:54 -0500

    add reduc-thunk and create action types

--- client/store.js
+++ client/store.js
@@ -1,9 +1,10 @@
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+//import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers/index.js';
 
 const store = createStore(
   reducers,
-  composeWithDevTools(),
+  applyMiddleware(thunk),
 );
 export default store;
