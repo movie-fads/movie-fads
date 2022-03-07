fd725df4194591a947d4bbbe17dc351439fee74e
Alex Corlin <alex.corlin6@gmail.com>
2022-03-07 09:34:20 -0800

    Ready to merge soon

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,10 +2,8 @@ const UserDb = require('../model/mediaModel.js');
 
 const userController = {
   // createUser - Add username, Add arrMediaObj as an empty array (POST)
-  // UserDb.create()
   async createUser(req, res, next) {
     try {
-      //take username from req body, send POST to  mongo with that username and empty arr for mediaObj
       const result = await UserDb.create({ username: req.body.username });
       res.locals.createUser = result;
       return next();
@@ -33,8 +31,6 @@ const userController = {
   },
 
   // removeUser - Delete User; based on username or maybe MongoID; TBD (DELETE)
-  // UserDb.deleteOne(username)
-
   async deleteUser(req, res, next) {
     try {
       const result = await UserDb.findOneAndDelete({
@@ -51,17 +47,13 @@ const userController = {
     }
   },
   // addMedia - Add an object to User's .arrMediaObj | PUT .findOneAndUpdate()
-  // UserDb.findOneAndUpdate(username, access arrMediaObj)
   /*
-  DOCUMENT:
-
   {
     TMDBId: 12
     haveSeen: false
     toWatch: true
     fav: true
   }
- 
     */
   async addMedia(req, res, next) {
     //req.body is { TMDBid: 70, fav: true }
@@ -88,24 +80,8 @@ const userController = {
       });
     }
   },
-  // updateWatchHistory - starts false, updates to true on watch, then must stay true (PATCH)
-  // UserDb.findOneAndUpdate(username, arrMediaObj)
 
-  //{
-  //username: req.params.username, arrMediaObj: { $elemMatch: { movieId: {strictly equal to the TMBDId we receive}}}
-  // }
-
-  /*
-  {
-    TMDBid: 70,
-    toWatch: true,
-    haveSeen: true,
-    fav: true, 
-
-  }
-   */
-  // { fav: true }
-
+  // UPDATE MEDIA (PUT)
   async updateMedia(req, res, next) {
     // { TMDBid: 70, fav: true }
     try {
@@ -149,15 +125,6 @@ const userController = {
       });
     }
   },
-  //findOneAndUpdate takes (filter object, object of property to update, object indicating if it should return new
-  // or old, also if it should upsert)
-  // updateToWatchList - What they want to watch, Starts false, updates to true. Can toggle.  (PATCH)
-  // UserDb.findOneAndUpdate
-  // const newBool = typeof arrMediaObj.toWatch ? false : true
-  // updateFavoriteStatus - favorite media items, toggle-able from T to F etc. (PATCH)
-  // UserDb.
-  // removeMedia - Remove an object from User's .arrMediaObj | (DELETE)
-  // UserDb.
 };
 
 module.exports = userController;
