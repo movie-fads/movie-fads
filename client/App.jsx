01231f6e2eeebbc94d77e1d9e3c74d236097444c
lunaf-github <lunaf91@gmail.com>
2022-03-07 09:12:35 -0800

    Created Main and Body Container, modified App component to render the BodyContainer component

--- client/App.jsx
+++ client/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import CardContainer from './containers/CardContainer.jsx';
+import MainContainer from './containers/MainContainer.jsx';
 
 const App = () => {
   return(
    <div> 
-     <h1>movie-fads</h1>
-     <h2>watchlist</h2>
-     <CardContainer />
-     <h2>favorites</h2>
-     <CardContainer />
-     <h2>recently watched</h2>
-     <CardContainer />
+    <MainContainer />
    </div>
   );
 };
