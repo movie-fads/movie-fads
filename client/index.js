690fe1310b6b60cbf77a90fa8219678e3399ac7c
david <github@djs.gg>
2022-03-07 11:53:15 -0500

    redux shell working

--- client/index.js
+++ client/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import App from './App.jsx'
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App.jsx';
+import store from './store.js';
 import styles from './styles/styles.css';
-ReactDOM.render(<App/>, document.querySelector('#root'));
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
