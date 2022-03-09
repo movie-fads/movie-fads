b2823a2f9e630cacc8d52a77e8a95dfc6b1a6c81
Janson Xavier <jansonxavier15@gmail.com>
2022-03-09 17:21:35 -0500

    remove err in movie controller

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -7,7 +7,7 @@ movieController.addFavorite = (req, res, next)=> {
   if (!id || !userId) {
       return next({ message: {err: 'There was an error' }, log: 'you did not input a valid id or userid'})
   }
-  UserDb.findOneAndUpdate({username: userId }, {} (err, user ) => {})
+  UserDb.findOneAndUpdate({username: userId }, {}, (err, user ) => {})
 }
 
 export default movieController;
