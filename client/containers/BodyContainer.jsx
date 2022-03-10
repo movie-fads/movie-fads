9f3857aac911945e5fc409dba00009e98bd83bb9
Janson Xavier <jansonxavier15@gmail.com>
2022-03-10 14:20:53 -0500

    working on finishing touches

--- client/containers/BodyContainer.jsx
+++ client/containers/BodyContainer.jsx
@@ -8,7 +8,73 @@ const mapStateToProps = (state) => ({
 });
 
 const BodyContainer = (props) => {
-  const [movie, setMovie] = useState('');
+  const [searchBarMovies, setSearchBarMovies] = useState([]);
+
+  console.log('movieList',props.movieList)
+
+  const genreCache = {};
+  let max; 
+
+  for(let i = 0; i < props.movieList.length; i++) {
+    if(props.movieList[i].fav === true) {
+      fetch(`https://api.themoviedb.org/3/movie/${props.movieList[i].TMDBid}?api_key=b77e6bcb363054a49df8e588ecd2fdc6`)
+      .then((res) => res.json())
+      .then(movieData => {
+        console.log('Hi Im in the movieDATA FETCH!', movieData.genres)
+        for(let i = 0; i < movieData.genres.length; i++) {
+          if(genreCache.hasOwnProperty(movieData.genres[i].name)){
+            genreCache[movieData.genres[i]]++ 
+          }
+          else genreCache[movieData.genres[i].name] = 1;
+          console.log(genreCache, 'genreCache')
+        }
+        for(const key in genreCache) {
+          console.log('here in object iteration')
+          if(!max) max = key;
+          else if(genreCache[key] > genreCache[max]) {
+            max = key;
+          }
+          console.log(max)
+        }
+      }).then(
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=b77e6bcb363054a49df8e588ecd2fdc6&language=en-US&sort_by=popularity.desc&with_genres=${max}`)
+        .then((res) => res.json())
+      .then(movieData => {
+        movieData.results[0].id
+      })
+      .catch((err) => console.log('failed fetch:', err))
+
+      )
+      .catch((err) => console.log('failed fetch:', err))
+    }
+  }
+  
+
+
+  // "results": [
+  //   {
+  //       "adult": false,
+  //       "backdrop_path": "/iQFcwSGbZXMkeyKrxbPnwnRo5fl.jpg",
+  //       "genre_ids": [
+  //           28,
+  //           12,
+  //           878
+  //       ],
+  //       "id": 634649,
+  //       "original_language": "en",
+  //       "original_title": "Spider-Man: No Way Home",
+  //       "overview": "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
+  //       "popularity": 4995.766,
+  //       "poster_path": "/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+  //       "release_date": "2021-12-15",
+  //       "title": "Spider-Man: No Way Home",
+  //       "video": false,
+  //       "vote_average": 8.3,
+  //       "vote_count": 9007
+  //   }
+
+
+  
 
   const handleKeyUp = (e) => {
     //event.charCode === 13 handle's clicking enter in search bar
@@ -21,10 +87,10 @@ const BodyContainer = (props) => {
         .then((res) => res.json())
         .then(data => {
           // console.log('incoming data', data.results[0])
-          if (!data.results[0]) {
+          if (!data.results.length) {
             document.querySelector('.search-bar').value=''; 
             alert(`invalid entry`);
-          } else setMovie(data.results[0])
+          } else setMovies(data.results.slice(0, 3))
         })
         .catch((err) => console.log('failed fetch:', err))
     }
@@ -42,11 +108,12 @@ const BodyContainer = (props) => {
       ></input>
       <button id="searchDeleteBtn" onClick={()=> {
         document.querySelector('.search-bar').value = '';
-        setMovie("")
+        setSearchBarMovies([])
       }}>X</button> 
       
-
-      {movie !== '' ? <SearchBarMedia key={`searchMovie`} tmdbId={movie.id} /> : null}
+      <div className="card-container">
+        {searchBarMovies.length !== 0 ? searchBarMovies.map((movie, i) => <SearchBarMedia key={`searchMovie${i}`} tmdbId={movie.id} />) : null}
+      </div>
 
       <h1>Watchlist</h1>
       <CardContainer
