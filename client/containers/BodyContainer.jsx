5aa09ab53d01efb0b12e580a34de6a4a07d8fc50
Kris Sorensen <Krismsorensen@gmail.com>
2022-03-10 16:05:43 -0500

    Search Bar and logo added

--- client/containers/BodyContainer.jsx
+++ client/containers/BodyContainer.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import CardContainer from "./CardContainer.jsx";
-import SearchBarMedia from '../components/SearchBarMedia.jsx';
+import SearchBarMedia from "../components/SearchBarMedia.jsx";
 
 const mapStateToProps = (state) => ({
   movieList: state.lists.userMovieArray,
@@ -10,46 +10,47 @@ const mapStateToProps = (state) => ({
 const BodyContainer = (props) => {
   const [searchBarMovies, setSearchBarMovies] = useState([]);
 
-  console.log('movieList',props.movieList)
+  console.log("movieList", props.movieList);
 
   const genreCache = {};
-  let max; 
+  let max;
 
-  for(let i = 0; i < props.movieList.length; i++) {
-    if(props.movieList[i].fav === true) {
-      fetch(`https://api.themoviedb.org/3/movie/${props.movieList[i].TMDBid}?api_key=b77e6bcb363054a49df8e588ecd2fdc6`)
-      .then((res) => res.json())
-      .then(movieData => {
-        console.log('Hi Im in the movieDATA FETCH!', movieData.genres)
-        for(let i = 0; i < movieData.genres.length; i++) {
-          if(genreCache.hasOwnProperty(movieData.genres[i].name)){
-            genreCache[movieData.genres[i]]++ 
-          }
-          else genreCache[movieData.genres[i].name] = 1;
-          console.log(genreCache, 'genreCache')
-        }
-        for(const key in genreCache) {
-          console.log('here in object iteration')
-          if(!max) max = key;
-          else if(genreCache[key] > genreCache[max]) {
-            max = key;
-          }
-          console.log(max)
-        }
-      }).then(
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=b77e6bcb363054a49df8e588ecd2fdc6&language=en-US&sort_by=popularity.desc&with_genres=${max}`)
-        .then((res) => res.json())
-      .then(movieData => {
-        movieData.results[0].id
-      })
-      .catch((err) => console.log('failed fetch:', err))
-
+  for (let i = 0; i < props.movieList.length; i++) {
+    if (props.movieList[i].fav === true) {
+      fetch(
+        `https://api.themoviedb.org/3/movie/${props.movieList[i].TMDBid}?api_key=b77e6bcb363054a49df8e588ecd2fdc6`
       )
-      .catch((err) => console.log('failed fetch:', err))
+        .then((res) => res.json())
+        .then((movieData) => {
+          console.log("Hi Im in the movieDATA FETCH!", movieData.genres);
+          for (let i = 0; i < movieData.genres.length; i++) {
+            if (genreCache.hasOwnProperty(movieData.genres[i].name)) {
+              genreCache[movieData.genres[i]]++;
+            } else genreCache[movieData.genres[i].name] = 1;
+            console.log(genreCache, "genreCache");
+          }
+          for (const key in genreCache) {
+            console.log("here in object iteration");
+            if (!max) max = key;
+            else if (genreCache[key] > genreCache[max]) {
+              max = key;
+            }
+            console.log(max);
+          }
+        })
+        .then(
+          fetch(
+            `https://api.themoviedb.org/3/discover/movie?api_key=b77e6bcb363054a49df8e588ecd2fdc6&language=en-US&sort_by=popularity.desc&with_genres=${max}`
+          )
+            .then((res) => res.json())
+            .then((movieData) => {
+              movieData.results[0].id;
+            })
+            .catch((err) => console.log("failed fetch:", err))
+        )
+        .catch((err) => console.log("failed fetch:", err));
     }
   }
-  
-
 
   // "results": [
   //   {
@@ -73,9 +74,6 @@ const BodyContainer = (props) => {
   //       "vote_count": 9007
   //   }
 
-
-  
-
   const handleKeyUp = (e) => {
     //event.charCode === 13 handle's clicking enter in search bar
     if (e.keyCode == 13) {
@@ -83,16 +81,18 @@ const BodyContainer = (props) => {
       // console.log(`what was typed in search: ${e.target.value}`);
       const movieTitle = e.target.value;
 
-      fetch(`https://api.themoviedb.org/3/search/movie?api_key=b77e6bcb363054a49df8e588ecd2fdc6&query=${movieTitle}`)
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=b77e6bcb363054a49df8e588ecd2fdc6&query=${movieTitle}`
+      )
         .then((res) => res.json())
-        .then(data => {
+        .then((data) => {
           // console.log('incoming data', data.results[0])
           if (!data.results.length) {
-            document.querySelector('.search-bar').value=''; 
+            document.querySelector(".search-bar").value = "";
             alert(`invalid entry`);
-          } else setMovies(data.results.slice(0, 3))
+          } else setMovies(data.results.slice(0, 3));
         })
-        .catch((err) => console.log('failed fetch:', err))
+        .catch((err) => console.log("failed fetch:", err));
     }
   };
 
@@ -100,19 +100,29 @@ const BodyContainer = (props) => {
   return (
     // * Need an event handler to handle search queries
     <div className="body-container">
+      <h2 className="searchHeader">Search for Movies</h2>
       <input
         className="search-bar"
-        placeholder="Search..."
+        placeholder="Search for Movies..."
         onKeyUp={(e) => handleKeyUp(e)}
         autoFocus
       ></input>
-      <button id="searchDeleteBtn" onClick={()=> {
-        document.querySelector('.search-bar').value = '';
-        setSearchBarMovies([])
-      }}>X</button> 
-      
+      <button
+        id="searchDeleteBtn"
+        onClick={() => {
+          document.querySelector(".search-bar").value = "";
+          setSearchBarMovies([]);
+        }}
+      >
+        X
+      </button>
+
       <div className="card-container">
-        {searchBarMovies.length !== 0 ? searchBarMovies.map((movie, i) => <SearchBarMedia key={`searchMovie${i}`} tmdbId={movie.id} />) : null}
+        {searchBarMovies.length !== 0
+          ? searchBarMovies.map((movie, i) => (
+              <SearchBarMedia key={`searchMovie${i}`} tmdbId={movie.id} />
+            ))
+          : null}
       </div>
 
       <h1>Watchlist</h1>
@@ -124,7 +134,8 @@ const BodyContainer = (props) => {
       <h1>Favorites</h1>
       <CardContainer
         movieList={props.movieList.filter((movie) => movie.fav === true)}
-      currRow="fav" />
+        currRow="fav"
+      />
       <h1>Recently Watched</h1>
       <CardContainer
         movieList={props.movieList.filter((movie) => movie.haveSeen === true)}
